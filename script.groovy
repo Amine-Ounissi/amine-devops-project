5011888820cc33a6f7644519147657053a8a0784
Amine Ounissi <73407173+Amine-Ounissi@users.noreply.github.com>
2022-09-07 17:26:54 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -14,6 +14,13 @@ def buildImage() {
 
 def deployApp() {
   echo 'deploying the application...'
+  withKubeConfig([credentialsId: 'kubeconfig-raw', serverUrl: '']) {
+      sh 'kubectl apply -f deployment.yaml'
+      sh 'kubectl get deployment'
+      sh 'kubectl get pods'
+      sh 'kubectl get svc'
+    }
 }
+
 
 return this
